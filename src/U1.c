28ad9da6ac4dd568e9f0a44e4733fd4fd1f6a5b6
Andre_G <andre3000pt@gmail.com>
2020-04-29 20:46:47 +0100

    Comments and changes to pid/threadID

--- src/U1.c
+++ src/U1.c
@@ -19,111 +19,107 @@
 // Interval betweeen User Requests (miliseconds)
 #define INTMS 50
 
-int i; //global variable 
-pthread_mutex_t mut=PTHREAD_MUTEX_INITIALIZER; 
+//global variables
+int i;  /**< número sequencial do pedido */
+pthread_mutex_t mut=PTHREAD_MUTEX_INITIALIZER; /**<  mutex para aceder a i*/
 
+
+/**
+ * Thread Function that creates requests
+ */
 void * thread_func(void *arg){
-    pthread_mutex_lock(&mut); 
-    i++;
-    pthread_mutex_unlock(&mut);
+  // updating i with mutex's
+  pthread_mutex_lock(&mut); 
+  i++;
+  pthread_mutex_unlock(&mut);
 
-    //printf("Client created thread\n");
-    int fd_pub;
-    char request[BUFSIZE];
-    char * fifopath = (char *) arg;
+  int fd_pub; /**< file descriptor of public fifo */
+  char request[BUFSIZE]; /**< Request string to send to public fifo*/
+  char * fifopath = (char *) arg; /**< public fifo path*/
+  int useTime = (rand() % (UPPERB - LOWERB + 1)) + LOWERB; /**< Determine the use time for this client */
 
-    int useTime = (rand() % (UPPERB - LOWERB + 1)) + LOWERB;  //random useTime between 1 and 100
-
-    fd_pub = open(fifopath,O_WRONLY);
-    if (fd_pub==-1){
-        printRegister(elapsedTime(), i, (int)getpid(), (long int)pthread_self(), useTime, -1, CLOSD);
-        pthread_exit((void*)1);
-    }
-
-    sprintf(request,"[ %d, %d, %lu, %d, -1 ]", i, getpid(), pthread_self(), useTime);
-    
-    if (write(fd_pub, &request, BUFSIZE)<0){perror("Error writing request: "); exit(1);}
-
-    printRegister(elapsedTime(), i, getpid(), pthread_self(), useTime, -1, IWANT);
-    close(fd_pub);
-    
-    char privateFifo[BUFSIZE]="tmp/";
-    char temp[BUFSIZE];
-    sprintf(temp,"%d",(int)getpid());
-    strcat(privateFifo,temp);
-    strcat(privateFifo,".");
-    sprintf(temp,"%ld",(long int)pthread_self());
-    strcat(privateFifo,temp);
-
-    //create private fifo to read message from server
-    if(mkfifo(privateFifo,0660)<0){perror("Error creating private FIFO:"); exit(1);}
-
-    int fd_priv = open(privateFifo, O_RDONLY);
-    if (fd_priv < 0) {perror("[Client]Error opening private FIFO: "); exit(1);}
-
-    char receivedMessage[BUFSIZE];
-    int tmpresult = read(fd_priv,&receivedMessage,BUFSIZE);
-
-    while(tmpresult<=0){
-      // lê de fifo privado continuamente enquanto não há info
-      tmpresult = read(fd_priv,&receivedMessage,BUFSIZE);
-    }
-    if(tmpresult<0) {
-      printRegister(elapsedTime(), i, (int)getpid(), (long int)pthread_self(), useTime, -1, FAILD);
-    }
-    int threadi, pid, dur, place;
-    long int tid;
-    sscanf(receivedMessage,"[ %d, %d, %ld, %d, %d ]",&threadi, &pid, &tid, &dur, &place);
-    if (place >= 0)
-      printRegister(elapsedTime(), threadi, getpid(), tid, dur, place, IAMIN);
-    else
-      printRegister(elapsedTime(), threadi, getpid(), tid, dur, place, CLOSD);
-    
-    close(fd_priv);
-    unlink(privateFifo);
-
+  // opening Public fifo to be able to write
+  fd_pub = open(fifopath,O_WRONLY);
+  if (fd_pub==-1){
+    // In case there's an error, the Server is Offline (Closed)
+    printRegister(elapsedTime(), i, (int)getpid(), (long int)pthread_self(), useTime, -1, CLOSD);
     pthread_exit(NULL);
+  }
 
+  // Making of message to send, writing of message and closing of fifo
+  sprintf(request,"[ %d, %d, %lu, %d, -1 ]", i, getpid(), pthread_self(), useTime);
+  if (write(fd_pub, &request, BUFSIZE)<0){perror("Error writing request: "); exit(1);}
+  printRegister(elapsedTime(), i, getpid(), pthread_self(), useTime, -1, IWANT);
+  close(fd_pub);
+  
+  // Making of pathname of private fifo 
+  char privateFifo[BUFSIZE]="tmp/";
+  char temp[BUFSIZE];
+  sprintf(temp,"%d",(int)getpid());
+  strcat(privateFifo,temp);
+  strcat(privateFifo,".");
+  sprintf(temp,"%ld",(long int)pthread_self());
+  strcat(privateFifo,temp);
+
+  //Create private fifo to read message from server
+  if(mkfifo(privateFifo,0660)<0){perror("Error creating private FIFO:"); exit(1);}
+
+  // Opening private fifo to read the response
+  int fd_priv = open(privateFifo, O_RDONLY);
+  if (fd_priv < 0) {perror("[Client]Error opening private FIFO: "); exit(1);}
+
+  // Attempting to read the response
+  char receivedMessage[BUFSIZE];
+  int tmpresult = read(fd_priv,&receivedMessage,BUFSIZE);
+  // Attempts to read from private fifo until there's a response
+  while(tmpresult<=0){tmpresult = read(fd_priv,&receivedMessage,BUFSIZE);}
+  if(tmpresult<0) {printRegister(elapsedTime(), i, getpid(), pthread_self(), useTime, -1, FAILD);}
+
+  // If there's a response, parse the response to different variables
+  int threadi, pid, dur, place;
+  long int tid;
+  sscanf(receivedMessage,"[ %d, %d, %ld, %d, %d ]",&threadi, &pid, &tid, &dur, &place);
+
+  // check if there's a place available or the server is closed
+  if (place >= 0)
+    printRegister(elapsedTime(), threadi, getpid(), pthread_self(), dur, place, IAMIN);
+  else
+    printRegister(elapsedTime(), threadi, getpid(), pthread_self(), dur, place, CLOSD);
+  
+  // cleanup
+  close(fd_priv);
+  unlink(privateFifo);
+  pthread_exit(NULL);
 }
 
 int main(int argc, char* argv[], char *envp[]) {
-    char fifoname[BUFSIZE];
-    char fifopath[BUFSIZE]="tmp/";
-    double nsecs;
-    pthread_t threads[THREADS_MAX];
-    int thr=0;
+  char fifoname[BUFSIZE];   /**< public fifo file name */
+  char fifopath[BUFSIZE]="tmp/";  /**< public fifo path */
+  double nsecs;  /**< numbers of seconds the program will be running */
+  pthread_t threads[THREADS_MAX];  /**< array to store thread id's */
+  int thr=0; /**< index for thread id / nº od threads created */
 
-    if (argc!=4) {
-        printf("Usage: U1 <-t secs> fifoname\n");
-        exit(1);
-    }
-    
-    //read arguments
-    strcpy(fifoname,argv[3]);
-    nsecs=atoi(argv[2])*1000;
-    strcat(fifopath,fifoname);
+  //check arguments
+  if (argc!=4) {
+    printf("Usage: U1 <-t secs> fifoname\n");
+    exit(1);}
+  
+  //read arguments
+  strcpy(fifoname,argv[3]);
+  nsecs=atoi(argv[2])*1000;
+  strcat(fifopath,fifoname);
 
-    /*printf("argv[0]: %s\n", argv[0]);
-    printf("argv[1]: %s\n", argv[1]);
-    printf("argv[2]: %s\n", argv[2]);
-    printf("argv[3]: %s\n", argv[3]);
+  //start counting time
+  startTime();
+  srand(time(NULL));
 
-    printf("Register format:\n");
-    printRegister(0.5, 23, 132, 135, 20000, 10, IWANT);*/
-
-    //start counting time
-    startTime();
-
-    srand(time(NULL));
-
-    //ciclo de geracao de pedidos
-    while(elapsedTime() < (double) nsecs){
-        pthread_create(&threads[thr],NULL, thread_func, (void *)fifopath);
-        pthread_detach(threads[thr]);
-        thr++;
-        usleep(INTMS*1000);
-    }
-    
-    //printf("Client exiting\n");
-    pthread_exit((void*)0);
+  //ciclo de geracao de pedidos
+  while(elapsedTime() < (double) nsecs){
+    pthread_create(&threads[thr],NULL, thread_func, (void *)fifopath);
+    pthread_detach(threads[thr]);
+    thr++;
+    usleep(INTMS*1000);
+  }
+  
+  pthread_exit((void*)0);
 }
