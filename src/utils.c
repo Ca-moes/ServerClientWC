143bb50b0b5e8685d4a0c35702c368c9810adeb0
Andre_G <andre3000pt@gmail.com>
2020-05-02 11:10:44 +0100

    white-spaces

--- src/utils.c
+++ src/utils.c
@@ -4,14 +4,10 @@ struct timespec start_time;
 struct timespec current_time;
 
 void startTime(){
-  if(clock_gettime(CLOCK_MONOTONIC, &start_time) == -1){
-    perror("startTime");
-  }
+  if(clock_gettime(CLOCK_MONOTONIC, &start_time) == -1){perror("startTime");}
 }
 
 double elapsedTime(){
-  if(clock_gettime(CLOCK_MONOTONIC, &current_time) == -1){
-    perror("elapsedTime");
-  }
+  if(clock_gettime(CLOCK_MONOTONIC, &current_time) == -1){perror("elapsedTime");}
   return (current_time.tv_sec-start_time.tv_sec)*1000+((current_time.tv_nsec-start_time.tv_nsec)/10e6);
 }
