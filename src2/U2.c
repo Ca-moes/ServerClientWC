9bbbbd8b8e2e2303ed1c4cd188068b17a9c8da93
filipeeteixeira <filipeteixeira00@hotmail.com>
2020-05-09 00:18:40 +0100

    fixed thread number bug

--- src2/U2.c
+++ src2/U2.c
@@ -33,8 +33,9 @@ pthread_mutex_t mut2=PTHREAD_MUTEX_INITIALIZER; /**<  mutex para aceder a i*/
  */
 void * thread_func(void *arg){
   // updating i with mutex's
+  int threadn;/**< thread number */
   if(pthread_mutex_lock(&mut2)!=0){perror("Client-MutexLock");}
-  i++;
+  threadn = i++;
   if(pthread_mutex_unlock(&mut2)!=0){perror("Client-MutexUnLock");}
 
   int fd_pub; /**< file descriptor of public fifo */
@@ -50,9 +51,8 @@ void * thread_func(void *arg){
     attempt++;
   } while (fd_pub==-1 && attempt < 5);
   if (fd_pub == -1) {
-    if(pthread_mutex_lock(&mut2)!=0){perror("Client-MutexLock");}
-    printRegister(time(NULL), i, getpid(), pthread_self(), useTime, -1,  CLOSD);
-    if(pthread_mutex_unlock(&mut2)!=0){perror("Client-MutexUnLock");}
+    printRegister(time(NULL), threadn, getpid(), pthread_self(), useTime, -1,  CLOSD);
+
     if(pthread_mutex_lock(&mut)!=0){perror("Client-MutexLock");}
     serverOpen.x = 0;
     if(pthread_mutex_unlock(&mut)!=0){perror("Client-MutexUnLock");}
@@ -60,7 +60,7 @@ void * thread_func(void *arg){
   }
 
   // Making of message to send, writing of message and closing of fifo
-  if(sprintf(request,"[ %d, %d, %lu, %d, -1 ]", i, getpid(), pthread_self(), useTime)<0){perror("Client-sprintf");}
+  if(sprintf(request,"[ %d, %d, %lu, %d, -1 ]", threadn, getpid(), pthread_self(), useTime)<0){perror("Client-sprintf");}
   
   if (write(fd_pub, &request, BUFSIZE)<0){
       perror("Error writing request: ");
@@ -68,7 +68,7 @@ void * thread_func(void *arg){
       pthread_exit(NULL);
   }
 
-  printRegister(time(NULL), i, getpid(), pthread_self(), useTime, -1, IWANT);
+  printRegister(time(NULL), threadn, getpid(), pthread_self(), useTime, -1, IWANT);
   if(close(fd_pub)==-1){perror("Client-closePublicFifo");}
   
   // Making of pathname of private fifo 
@@ -90,7 +90,7 @@ void * thread_func(void *arg){
     fd_priv = open(privateFifo, O_RDONLY | O_NONBLOCK);
   } while (fd_priv==-1 && elapsedTime() - startt < MSATTEMPT);
   if (fd_priv < 0) {
-    if(fprintf(stderr, "%d.%s\n", i, "Client - Error Opening Private Fifo")<0){perror("Client-fprintf");}
+    if(fprintf(stderr, "%d.%s\n", threadn, "Client - Error Opening Private Fifo")<0){perror("Client-fprintf");}
     if(close(fd_priv)==-1){perror("Client-closePrivateFifo");}
     pthread_exit(NULL);
   }
@@ -108,7 +108,8 @@ void * thread_func(void *arg){
     try++;
   }
   if(tmpresult<=0) {
-    printRegister(time(NULL), i, getpid(), pthread_self(), useTime, -1, FAILD);
+    printRegister(time(NULL), threadn, getpid(), pthread_self(), useTime, -1, FAILD);
+
     if(close(fd_priv)==-1){perror("Client-closePrivateFifo");}
     if (unlink(privateFifo)==-1){perror("Error destroying private fifo:");}
     pthread_exit(0);
