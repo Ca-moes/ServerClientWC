b05541da69764a57e788296b275aa7d32c1d9e30
Andre_G <andre3000pt@gmail.com>
2020-05-09 12:41:19 +0100

    strat draft PR

--- removefifo.bash
+++ removefifo.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd src2/tmp
 rm fifoname
+# De acordo com as leis de Mendel,indivíduos com genótipo:a) AaBb produzem gametas A, B, a e b.b) AaBB produzem gametas AB e aB.c) Aa produzem gametas AA, Aa e aa.d) AA produzem gametas AA.e) AABB produzem dois tipos de gametas.
